85bd7bd1609040294409827c72249d7cc72aba26
Myint Myint Zaw <myintmyintzaw@advent-soft.com>
2020-09-24 16:38:42 +0630

    Read Data Cloud FireStore

--- app/src/main/java/com/padcx/mmz/grocery/network/CloudFirestoreFirebaseApiImpl.kt
+++ app/src/main/java/com/padcx/mmz/grocery/network/CloudFirestoreFirebaseApiImpl.kt
@@ -1,15 +1,36 @@
 package com.padcx.mmz.grocery.network
 
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.padcx.mmz.grocery.data.vos.GroceryVO
 
 /**
  * Created by Myint Myint Zaw on 9/24/2020.
  */
-class CloudFirestoreFirebaseApiImpl : FirebaseApi {
+object CloudFirestoreFirebaseApiImpl : FirebaseApi {
+
+    val db = Firebase.firestore
     override fun getGroceries(
         onSuccess: (groceries: List<GroceryVO>) -> Unit,
         onFialure: (String) -> Unit
     ) {
+        db.collection("groceries")
+            .get()
+            .addOnSuccessListener { result ->
+                val groceriesList: MutableList<GroceryVO> = arrayListOf()
+                for (document in result) {
+                    val data = document.data
+                    var grocery = GroceryVO()
+                    grocery.name = data["name"] as String
+                    grocery.description = data["description"] as String
+                    grocery.amount = (data["amount"] as Long).toInt()
+                    groceriesList.add(grocery)
+                }
+                onSuccess(groceriesList)
+            }
+            .addOnFailureListener { exception ->
+                onFialure(exception.message ?: "Please check connection")
+            }
 
     }
 
